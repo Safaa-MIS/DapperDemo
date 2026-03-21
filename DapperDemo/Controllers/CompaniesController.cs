12f8650fd0e404b829caf45bb2481d4321d98efb
Safaa-MIS <safaaabdelhamidsoliman@gmail.com>
2026-03-21 12:24:05 +0400

    Add project files.

---
+++ DapperDemo/Controllers/CompaniesController.cs
@@ -0,0 +1,153 @@
+﻿using DapperDemo.Data;
+using DapperDemo.Models;
+using DapperDemo.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperDemo.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        private readonly ICompanyRepository _compRepo;
+
+        public CompaniesController(ICompanyRepository repo)
+        {
+            _compRepo = repo;
+        }
+
+        // GET: Companies
+        public IActionResult Index()
+        {
+            return View(_compRepo.GetAll());
+        }
+
+        // GET: Companies/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company = _compRepo.Find(id.Value);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
+        }
+
+        // GET: Companies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Companies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CompanyId,Name,Address,City,State,PostalCode")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                _compRepo.Add(company);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
+        }
+
+        // GET: Companies/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            var company = _compRepo.Find(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return View(company);
+        }
+
+        // POST: Companies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CompanyId,Name,Address,City,State,PostalCode")] Company company)
+        {
+            if (id != company.CompanyId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _compRepo.Update(company);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CompanyExists(company.CompanyId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
+        }
+
+        // GET: Companies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company = _compRepo.Find(id.Value);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
+        }
+
+        // POST: Companies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var company = _compRepo.Find(id);
+            if (company != null)
+            {
+                _compRepo.Remove(id);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CompanyExists(int id)
+        {
+            return _compRepo.Find(id) != null;
+        }
+    }
+}
---
+++ DapperDemo/Models/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperDemo.Models;
+    public class Company
+    {
+        public Company()
+        {
+            Employees = new List<Employee>();
+        }
+
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+    }
